131864a1e0178c1b2384209d8e04261852abc9f4
drew <spock128@gmail.com>
2023-10-31 07:35:30 -0700

    add lex and yacc outputs

---
+++ y_tab.h
@@ -0,0 +1,127 @@
+/* A Bison parser, made by GNU Bison 3.8.2.  */
+
+/* Bison interface for Yacc-like parsers in C
+
+   Copyright (C) 1984, 1989-1990, 2000-2015, 2018-2021 Free Software Foundation,
+   Inc.
+
+   This program is free software: you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation, either version 3 of the License, or
+   (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program.  If not, see <https://www.gnu.org/licenses/>.  */
+
+/* As a special exception, you may create a larger work that contains
+   part or all of the Bison parser skeleton and distribute that work
+   under terms of your choice, so long as that work isn't itself a
+   parser generator using the skeleton or a modified version thereof
+   as a parser skeleton.  Alternatively, if you modify or redistribute
+   the parser skeleton itself, you may (at your option) remove this
+   special exception, which will cause the skeleton and the resulting
+   Bison output files to be licensed under the GNU General Public
+   License without this special exception.
+
+   This special exception was added by the Free Software Foundation in
+   version 2.2 of Bison.  */
+
+/* DO NOT RELY ON FEATURES THAT ARE NOT DOCUMENTED in the manual,
+   especially those whose name start with YY_ or yy_.  They are
+   private implementation details that can be changed or removed.  */
+
+#ifndef YY_YY_Y_TAB_H_INCLUDED
+# define YY_YY_Y_TAB_H_INCLUDED
+/* Debug traces.  */
+#ifndef YYDEBUG
+# define YYDEBUG 0
+#endif
+#if YYDEBUG
+extern int yydebug;
+#endif
+
+/* Token kinds.  */
+#ifndef YYTOKENTYPE
+# define YYTOKENTYPE
+  enum yytokentype
+  {
+    YYEMPTY = -2,
+    YYEOF = 0,                     /* "end of file"  */
+    YYerror = 256,                 /* error  */
+    YYUNDEF = 257,                 /* "invalid token"  */
+    SYMBOL = 258,                  /* SYMBOL  */
+    VALUE = 259,                   /* VALUE  */
+    STRING = 260,                  /* STRING  */
+    EXIT = 261,                    /* EXIT  */
+    WHEN = 262,                    /* WHEN  */
+    STRING_DEF = 263,              /* STRING_DEF  */
+    TEXT = 264,                    /* TEXT  */
+    SHORT = 265,                   /* SHORT  */
+    CLEAR = 266,                   /* CLEAR  */
+    SET = 267,                     /* SET  */
+    GOTO = 268,                    /* GOTO  */
+    OPTIONS = 269,                 /* OPTIONS  */
+    AND = 270,                     /* AND  */
+    OR = 271,                      /* OR  */
+    NOT = 272,                     /* NOT  */
+    EQUALS = 273                   /* EQUALS  */
+  };
+  typedef enum yytokentype yytoken_kind_t;
+#endif
+/* Token kinds.  */
+#define YYEMPTY -2
+#define YYEOF 0
+#define YYerror 256
+#define YYUNDEF 257
+#define SYMBOL 258
+#define VALUE 259
+#define STRING 260
+#define EXIT 261
+#define WHEN 262
+#define STRING_DEF 263
+#define TEXT 264
+#define SHORT 265
+#define CLEAR 266
+#define SET 267
+#define GOTO 268
+#define OPTIONS 269
+#define AND 270
+#define OR 271
+#define NOT 272
+#define EQUALS 273
+
+/* Value type.  */
+#if ! defined YYSTYPE && ! defined YYSTYPE_IS_DECLARED
+union YYSTYPE
+{
+#line 12 "dlg.y"
+
+    int symbol;
+    int value;
+    int string;
+    struct expression* expression;
+    struct action* action;
+    struct setter* setter;
+    struct option* option;
+
+#line 113 "y_tab.h"
+
+};
+typedef union YYSTYPE YYSTYPE;
+# define YYSTYPE_IS_TRIVIAL 1
+# define YYSTYPE_IS_DECLARED 1
+#endif
+
+
+extern YYSTYPE yylval;
+
+
+int yyparse (void);
+
+
+#endif /* !YY_YY_Y_TAB_H_INCLUDED  */
