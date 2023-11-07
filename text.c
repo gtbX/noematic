de37aa9c0e57cc5d8ed8d099e28bcc6fbde0f49f
Andrew Galante <spock128@gmail.com>
2023-11-06 22:09:42 -0800

    value replacement in output strings

--- text.c
+++ text.c
@@ -5,31 +5,41 @@
 #include "strtable.h"
 #include "symtable.h"
 
-int read_sym_name(const char* str, char* buf) {
-    size_t n = 0;
-    while (*str && *str != '\'' && n < SYM_LEN - 1) {
-        *buf++ = *str++;
-        n++;
+int get_sym_var(const char** str) {
+    char delim = *++*str;
+    if (delim == '\'' || delim == '\"') {
+        char sym[SYM_LEN], *s = sym;
+        int* var;
+        size_t n = 0;
+        ++*str;
+        while (**str && **str != delim && n < SYM_LEN - 1) {
+            *s++ = *(*str)++;
+            n++;
+        }
+        *s = '\0';
+        if (**str == delim)
+            ++*str;
+        var = get_var(get_sym(sym));
+        if (var)
+            return *var;
     }
-    *buf = '\0';
-    return n;
+    return -1;
 }
 
 void print_text(const char* text) {
     while (*text) {
         if (*text == '%') {
             switch (*++text) {
+            case 'O':
+            case 'o':
+                printf("%d", get_sym_var(&text));
+                break;
+            case 'T':
             case 't':
-                if (*++text == '\'') {
-                    char sym[SYM_LEN];
-                    int *var;
-                    text++;
-                    text += read_sym_name(text, sym);
-                    if (*text == '\'')
-                        text++;
-                    var = get_var(get_sym(sym));
-                    print_text(get_string(var ? *var : -1));
-                }
+                print_text(get_string(get_sym_var(&text)));
+                break;
+            default:
+                putchar('%');
                 break;
             }
         } else {
