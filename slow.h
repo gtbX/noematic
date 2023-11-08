e068d2de816935f6fe27ee72a15ab7a0789b4754
Andrew Galante <spock128@gmail.com>
2023-11-07 16:14:05 -0800

    docs'

--- slow.h
+++ slow.h
@@ -1,8 +1,8 @@
-#define CPS 100
-#define WAIT_MS 100
+#define CPS 100 /* text output speed in chars per second */
+#define WAIT_MS 100 /* base wait time of %w1 command, in ms */
 
 /* delay for the requisite amount of time to hit the target CPS rate */
 void slow_char(int chars);
 
-/* wait for the given number of wait periods */
+/* wait for the given number of wait times */
 void wait(int n);
