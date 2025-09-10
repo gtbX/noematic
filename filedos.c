e030b95f2c6a9be01fe75b0b971dce5e32a1d79c
Andrew Galante <spock128@gmail.com>
2025-09-09 21:58:57 -0700

    format filedos.c

--- filedos.c
+++ filedos.c
@@ -7,31 +7,31 @@ char* get_filename(char* path) {
     int nlen = 0, elen = 0;
     int name = 1;
     while (*w = *r++) {
-	if (*w == '\\' || *w == '/') {
-	    if (*w == '/') *w = '\\';
-	    name = 1;
-	    nlen = 0;
-	    elen = 0;
-	} else if (name) {
-	    nlen++;
-	    if (*w == '.') {
-		name = 0;
-	    } else {
-		if (nlen >= 9) {
-		    if (nlen == 9) {
-			w[-2] = '~';
-			w[-1] = '1';
-		    }
-		    w--;
-		}
-	    }
-	} else {
-	    elen++;
-	    if (elen > 3) {
-		w--;
-	    }
-	}
-	w++;
+        if (*w == '\\' || *w == '/') {
+            if (*w == '/') *w = '\\';
+            name = 1;
+            nlen = 0;
+            elen = 0;
+        } else if (name) {
+            nlen++;
+            if (*w == '.') {
+                name = 0;
+            } else {
+                if (nlen >= 9) {
+                    if (nlen == 9) {
+                        w[-2] = '~';
+                        w[-1] = '1';
+                    }
+                    w--;
+                }
+            }
+        } else {
+            elen++;
+            if (elen > 3) {
+                w--;
+            }
+        }
+        w++;
     }
     return path;
 }
